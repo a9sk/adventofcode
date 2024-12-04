51b78e03a1055a17a2f94ad608b0a7a7fec31211
a9sk <920a9sk42f76c765@proton.me>
2024-12-04 22:02:30 +0100

    solution day 4 part 2

--- solutions/day-04/main.go
+++ solutions/day-04/main.go
@@ -48,16 +48,16 @@ func solvePart1(input string) string {
 			for j := 0; j < len(matrix[i])-3; j++ {
 				if matrix[i][j] == 'X' && matrix[i][j+1] == 'M' && matrix[i][j+2] == 'A' && matrix[i][j+3] == 'S' {
 					total++
-					fmt.Printf("found: %c %c %c %c\n", matrix[i][j], matrix[i][j+1], matrix[i][j+2], matrix[i][j+3])
+					// fmt.Printf("found: %c %c %c %c\n", matrix[i][j], matrix[i][j+1], matrix[i][j+2], matrix[i][j+3])
 				}
 
 				if i < len(matrix[i])-3 && matrix[i][j] == 'X' && matrix[i+1][j+1] == 'M' && matrix[i+2][j+2] == 'A' && matrix[i+3][j+3] == 'S' {
 					total++
-					fmt.Printf("found: %c %c %c %c\n", matrix[i][j], matrix[i+1][j+1], matrix[i+2][j+2], matrix[i+3][j+3])
+					// fmt.Printf("found: %c %c %c %c\n", matrix[i][j], matrix[i+1][j+1], matrix[i+2][j+2], matrix[i+3][j+3])
 				}
 			}
 		}
-		fmt.Println("ROTATE")
+		// fmt.Println("ROTATE")
 		matrix = utils.Rotate90(matrix)
 	}
 
@@ -65,6 +65,20 @@ func solvePart1(input string) string {
 }
 
 func solvePart2(input string) string {
+	parsedInput := utils.ParseLines(input)
+	matrix := utils.ParseGrid(parsedInput)
 
-	return "Solution for Part 2 not implemented"
+	total := 0
+
+	for i := 1; i < len(matrix)-1; i++ {
+		for j := 1; j < len(matrix[i])-1; j++ {
+			// very long if statement coming
+			if matrix[i][j] == 'A' && ((matrix[i-1][j-1] == 'S' && matrix[i+1][j+1] == 'M') || (matrix[i-1][j-1] == 'M' && matrix[i+1][j+1] == 'S')) && ((matrix[i+1][j-1] == 'S' && matrix[i-1][j+1] == 'M') || (matrix[i+1][j-1] == 'M' && matrix[i-1][j+1] == 'S')) {
+				total++
+				// fmt.Println("found an X-MAS")
+			}
+		}
+	}
+
+	return strconv.Itoa(total)
 }
