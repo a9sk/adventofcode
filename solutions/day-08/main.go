b1576bc59c163f95fb21ec9e52956d98b08e2760
a9sk <920a9sk42f76c765@proton.me>
2024-12-08 16:52:29 +0100

    solution day 8 part 2

--- solutions/day-08/main.go
+++ solutions/day-08/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	"github.com/a9sk/adventofcode/utils"
@@ -35,6 +36,13 @@ func main() {
 	resultPart2 := solvePart2(string(input))
 	fmt.Println("Solution Part 2:", resultPart2)
 }
+
+/*
+	NOTE: i could not solve todays part one, i just could not get the right output. I had to look for the solution
+	on the AoC reddit and found TimFanelle's Golang implementation. Looking at how he did it, i noticed i had read the
+	definition of antinode in the wrong way (i'll use the "language barrier" excuse here). After i noticed i just had to
+	fix a few things and came up with the following solution for part 1 (and then for part 2 obv).
+*/
 
 func solvePart1(input string) string {
 
@@ -82,10 +90,55 @@ func solvePart1(input string) string {
 		}
 	}
 
-	return fmt.Sprintf("%d", len(antinodes))
+	return strconv.Itoa(len(antinodes))
 }
 
 func solvePart2(input string) string {
 
-	return "Solution for Part 2 not implemented"
+	lines := utils.ParseLines(input)
+	height := len(lines)
+	width := len(lines[0])
+
+	antennas := make(map[rune][]struct{ x, y int })
+
+	for y, line := range lines {
+		for x, ch := range line {
+			if ch != '.' {
+				antennas[ch] = append(antennas[ch], struct{ x, y int }{x, y})
+			}
+		}
+	}
+
+	antinodes := make(map[struct{ x, y int }]bool)
+
+	for _, points := range antennas {
+		if len(points) < 2 {
+			continue
+		}
+
+		for i := 0; i < len(points)-1; i++ {
+			for j := i + 1; j < len(points); j++ {
+				a1, a2 := points[i], points[j]
+
+				distX := a2.x - a1.x
+				distY := a2.y - a1.y
+
+				currentNode := struct{ x, y int }{x: a2.x - distX, y: a2.y - distY}
+				for currentNode.x >= 0 && currentNode.x < width &&
+					currentNode.y >= 0 && currentNode.y < height {
+					antinodes[currentNode] = true
+					currentNode = struct{ x, y int }{x: currentNode.x - distX, y: currentNode.y - distY}
+				}
+
+				currentNode = struct{ x, y int }{x: a1.x + distX, y: a1.y + distY}
+				for currentNode.x >= 0 && currentNode.x < width &&
+					currentNode.y >= 0 && currentNode.y < height {
+					antinodes[currentNode] = true
+					currentNode = struct{ x, y int }{x: currentNode.x + distX, y: currentNode.y + distY}
+				}
+			}
+		}
+	}
+
+	return strconv.Itoa(len(antinodes))
 }
