8d11a8a75331d7d7d1a0053fc81cb1fed0a8f8c5
a9sk <920a9sk42f76c765@proton.me>
2024-12-18 20:51:08 +0100

    solution day 18 part 1

---
+++ solutions/day-18/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Println("Error opening input file:", err)
+		return
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading input:", err)
+		return
+	}
+
+	input := strings.Join(lines, "\n")
+
+	resultPart1 := solvePart1(string(input))
+	fmt.Println("Solution Part 1:", resultPart1)
+
+	resultPart2 := solvePart2(string(input))
+	fmt.Println("Solution Part 2:", resultPart2)
+}
+
+// i just now noticed that structs in go should have the fist letter in caps
+type Point struct {
+	x, y int
+}
+
+func solvePart1(input string) string {
+
+	bytes := []Point{}
+
+	for _, line := range strings.Split(input, "\n") {
+		var x, y int
+		fmt.Sscanf(line, "%d,%d", &x, &y)
+		bytes = append(bytes, Point{x: x, y: y})
+	}
+
+	field := map[Point]bool{}
+	for y := 0; y < 71; y++ {
+		for x := 0; x < 71; x++ {
+			field[Point{x: x, y: y}] = true
+		}
+	}
+
+mainFor:
+	for bite := range bytes { // i use bite instead of byte cos i do not like byte's colour being different
+		field[bytes[bite]] = false
+
+		queue, distance := []Point{{0, 0}}, map[Point]int{{0, 0}: 0}
+		for len(queue) > 0 {
+			position := queue[0]
+			queue = queue[1:]
+
+			if position == (Point{70, 70}) {
+				if bite == 1024 {
+					return strconv.Itoa(distance[position])
+				}
+				continue mainFor
+			}
+
+			for _, direction := range []Point{{0, -1}, {1, 0}, {0, 1}, {-1, 0}} {
+				new := Point{x: position.x + direction.x, y: position.y + direction.y}
+				if _, ok := distance[new]; !ok && field[new] {
+					queue, distance[new] = append(queue, new), distance[position]+1
+				}
+			}
+		}
+		break
+	}
+
+	return ""
+}
+
+func solvePart2(input string) string {
+
+	return "Solution for part 2 not implemented"
+}
