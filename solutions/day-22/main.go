58b1b9018ccc068db1fa29aae944385643191907
a9sk <920a9sk42f76c765@proton.me>
2024-12-22 17:57:12 +0100

    solution day 22 part 2

--- solutions/day-22/main.go
+++ solutions/day-22/main.go
@@ -58,5 +58,50 @@ func solvePart1(input string) string {
 
 func solvePart2(input string) string {
 
-	return "Solution for Part 2 not implemented"
+	numbers := strings.Split(input, "\n")
+
+	total := make(map[[4]int]int)
+
+	var sum = 0
+	for _, number := range numbers {
+		secret, _ := strconv.Atoi(number)
+		last := secret % 10
+		pList := make([][2]int, 0, 2000)
+
+		for i := 0; i < 2000; i++ {
+			helpSecret := secret * 64
+			secret = (secret ^ helpSecret) % 16777216
+			helpSecret = int(secret / 32)
+			secret = (secret ^ helpSecret) % 16777216
+			helpSecret = secret * 2048
+			secret = (secret ^ helpSecret) % 16777216
+			temp := secret % 10
+			pList = append(pList, [2]int{temp - last, temp})
+			last = temp
+		}
+
+		seen := make(map[[4]int]bool)
+
+		for i := 0; i < len(pList)-4; i++ {
+			var pattern [4]int
+			for j := 0; j < 4; j++ {
+				pattern[j] = pList[i+j][0]
+			}
+
+			val := pList[i+3][1]
+
+			if !seen[pattern] {
+				seen[pattern] = true
+				total[pattern] += val
+			}
+		}
+	}
+
+	for _, value := range total {
+		if value > sum {
+			sum = value
+		}
+	}
+
+	return strconv.Itoa(sum)
 }
