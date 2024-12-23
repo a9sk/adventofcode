6a5e1f3589b68e2c6ec7eda388ba0720561cdbc1
a9sk <920a9sk42f76c765@proton.me>
2024-12-23 19:14:48 +0100

    faster solution day 6 part 2

--- solutions/day-06/main.go
+++ solutions/day-06/main.go
@@ -70,15 +70,6 @@ func solvePart1(input string) string {
 
 	field, _ = recursiveMove(field, pos, "up")
 
-	// var isOut bool
-	// var direction = "up"
-	// for {
-	// 	if field, pos, direction, isOut = Move(field, pos, direction); isOut {
-	// 		field[pos.y][pos.x] = 'X' // to make sure the last cell visited is actually considered as X
-	// 		break
-	// 	}
-	// }
-
 	var sum = 0
 	for i := 0; i < len(field); i++ {
 		for j := 0; j < len(field[i]); j++ {
@@ -89,54 +80,6 @@ func solvePart1(input string) string {
 	}
 	return strconv.Itoa(sum)
 }
-
-// func Move(field [][]rune, pos position, direction string) ([][]rune, position, string, bool) {
-// 	switch direction {
-// 	case "up":
-// 		if pos.y-1 >= 0 && (field[pos.y-1][pos.x] == '.' || field[pos.y-1][pos.x] == 'X') { // len(field[pos.y]) > pos.x+1
-// 			field[pos.y][pos.x] = 'X'
-// 			newPos := position{pos.y - 1, pos.x}
-// 			return field, newPos, "up", false
-// 		} else if pos.y-1 >= 0 && field[pos.y-1][pos.x] == '#' {
-// 			field[pos.y][pos.x] = 'X'
-// 			return field, pos, "right", false
-// 		} else if pos.y-1 < 0 { // you are at the top of the map and you are going out...
-// 			return field, pos, "up", true
-// 		}
-// 	case "right":
-// 		if pos.x+1 < len(field[pos.y]) && (field[pos.y][pos.x+1] == '.' || field[pos.y][pos.x+1] == 'X') {
-// 			field[pos.y][pos.x] = 'X'
-// 			newPos := position{pos.y, pos.x + 1}
-// 			return field, newPos, "right", false
-// 		} else if pos.x+1 < len(field[pos.y]) && field[pos.y][pos.x+1] == '#' {
-// 			field[pos.y][pos.x] = 'X'
-// 			return field, pos, "down", false
-// 		} else if pos.x+1 == len(field[pos.y]) { // if you get to len() then you are out
-// 			return field, pos, "right", true
-// 		}
-// 	case "down":
-// 		if pos.y+1 < len(field) && (field[pos.y+1][pos.x] == '.' || field[pos.y+1][pos.x] == 'X') {
-// 			field[pos.y][pos.x] = 'X'
-// 			newPos := position{pos.y + 1, pos.x}
-// 			return field, newPos, "down", false
-// 		} else if pos.y+1 < len(field) && field[pos.y+1][pos.x] == '#' {
-// 			field[pos.y][pos.x] = 'X'
-// 			return field, pos, "left", false
-// 		} else if pos.y+1 == len(field) {
-// 			return field, pos, "down", true
-// 		}
-// 	case "left":
-// 		if pos.x-1 >= 0 && (field[pos.y][pos.x-1] == '.' || field[pos.y][pos.x-1] == 'X') {
-// 			field[pos.y][pos.x] = 'X'
-// 			newPos := position{pos.y, pos.x - 1}
-// 			return field, newPos, "left", false
-// 		} else if pos.x-1 >= 0 && field[pos.y][pos.x-1] == '#' {
-// 			field[pos.y][pos.x] = 'X'
-// 			return field, pos, "up", false
-// 		} else if pos.x == 0 {
-// 			return field, pos, "left", true
-// 		}
-// 	}
 
 func recursiveMove(field [][]rune, pos position, direction string) ([][]rune, bool) {
 	field[pos.y][pos.x] = 'X'
@@ -171,7 +114,6 @@ func recursiveMove(field [][]rune, pos position, direction string) ([][]rune, bo
 
 func solvePart2(input string) string {
 	restartField := utils.ParseGrid(utils.ParseLines(input))
-
 	var startPos position
 	for i := 0; i < len(restartField); i++ {
 		for j := 0; j < len(restartField[i]); j++ {
@@ -182,69 +124,109 @@ func solvePart2(input string) string {
 		}
 	}
 
+	baseReachable := make(map[position]bool)
+	traceBasePathIterative(restartField, startPos, baseReachable)
+
 	var sum = 0
-	for i := 0; i < len(restartField); i++ {
-		for j := 0; j < len(restartField[i]); j++ {
+	for pos := range baseReachable {
+		if restartField[pos.y][pos.x] == '#' || restartField[pos.y][pos.x] == '^' {
+			continue
+		}
 
-			field := make([][]rune, len(restartField))
-			for restartFieldRow := range restartField {
-				field[restartFieldRow] = append([]rune(nil), restartField[restartFieldRow]...)
-			}
-
-			if field[i][j] == '#' || field[i][j] == '^' {
-				continue
-			} else {
-				field[i][j] = '#'
-				seen := make([]posDir, len(restartField)*len(restartField[0])) // i will probably need to learn how to use maps one day...
-				if recursiveMoveLoopCheck(field, startPos, "up", seen) {
-					sum++
-				}
-			}
+		if checkLoopFast(restartField, startPos, pos) {
+			sum++
 		}
 	}
-
 	return strconv.Itoa(sum)
 }
 
-type posDir struct {
-	pos       position
-	direction string
+func traceBasePathIterative(field [][]rune, start position, reachable map[position]bool) {
+	type state struct {
+		pos       position
+		direction string
+	}
+
+	queue := []state{{pos: start, direction: "up"}}
+	visited := make(map[state]bool)
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if visited[current] {
+			continue
+		}
+		visited[current] = true
+		reachable[current.pos] = true
+
+		var dir struct {
+			name   string
+			dy, dx int
+			next   string
+		}
+		for _, d := range directions {
+			if d.name == current.direction {
+				dir = d
+				break
+			}
+		}
+
+		nextPos := position{current.pos.y + dir.dy, current.pos.x + dir.dx}
+		if nextPos.y < 0 || nextPos.y >= len(field) || nextPos.x < 0 || nextPos.x >= len(field[0]) {
+			continue
+		}
+
+		nextCell := field[nextPos.y][nextPos.x]
+		if nextCell == '.' || nextCell == '^' {
+			queue = append(queue, state{pos: nextPos, direction: current.direction})
+		} else if nextCell == '#' {
+			queue = append(queue, state{pos: current.pos, direction: dir.next})
+		}
+	}
 }
 
-// running this thing took me about 10 minutes... probably recursion isn't the best way
-func recursiveMoveLoopCheck(field [][]rune, pos position, direction string, seen []posDir) bool {
-	for _, i := range seen {
-		if i.pos == pos && i.direction == direction {
+func checkLoopFast(field [][]rune, start position, blocked position) bool {
+	type state struct {
+		pos       position
+		direction string
+	}
+
+	visited := make(map[state]bool)
+	current := state{pos: start, direction: "up"}
+
+	for {
+		if visited[current] {
 			return true
 		}
-	}
+		visited[current] = true
 
-	seen = append(seen, posDir{pos, direction})
+		var dir struct {
+			name   string
+			dy, dx int
+			next   string
+		}
+		for _, d := range directions {
+			if d.name == current.direction {
+				dir = d
+				break
+			}
+		}
 
-	var dir struct {
-		name   string
-		dy, dx int
-		next   string
-	}
-	for _, d := range directions {
-		if d.name == direction {
-			dir = d
-			break
+		nextPos := position{current.pos.y + dir.dy, current.pos.x + dir.dx}
+		if nextPos.y < 0 || nextPos.y >= len(field) || nextPos.x < 0 || nextPos.x >= len(field[0]) {
+			return false
+		}
+
+		if nextPos == blocked {
+			current.direction = dir.next
+			continue
+		}
+
+		nextCell := field[nextPos.y][nextPos.x]
+		if nextCell == '.' || nextCell == '^' {
+			current.pos = nextPos
+		} else if nextCell == '#' {
+			current.direction = dir.next
 		}
 	}
-
-	nextPos := position{pos.y + dir.dy, pos.x + dir.dx}
-
-	if nextPos.y < 0 || nextPos.y >= len(field) || nextPos.x < 0 || nextPos.x >= len(field[0]) {
-		return false
-	}
-
-	nextCell := field[nextPos.y][nextPos.x]
-	if nextCell == '.' || nextCell == '^' {
-		return recursiveMoveLoopCheck(field, nextPos, dir.name, seen)
-	} else if nextCell == '#' {
-		return recursiveMoveLoopCheck(field, pos, dir.next, seen)
-	}
-
-	return false
 }
