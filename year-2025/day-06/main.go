fed9727d97c6976e6944a346372eac1c450ee619
a9sk <emiliano.rizzonelli@proton.me>
2025-12-06 16:29:21 +0100

    solution day 6 part 2

--- year-2025/day-06/main.go
+++ year-2025/day-06/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	input, _ := os.ReadFile("input.txt")
 
-	rows := strings.Split(strings.TrimSpace(string(input)), "\n")
+	rows := strings.Split(strings.TrimRight(string(input), "\n"), "\n")
 	h := len(rows)
 
 	ops := []rune{}
@@ -20,32 +20,99 @@ func main() {
 
 	var nums [][]int
 	for _, row := range rows[:h-1] {
-		fields := strings.Fields(row)
-		arr := make([]int, len(fields))
-		for i, f := range fields {
+		fs := strings.Fields(row)
+		arr := make([]int, len(fs))
+		for i, f := range fs {
 			n, _ := strconv.Atoi(f)
 			arr[i] = n
 		}
 		nums = append(nums, arr)
 	}
 
-	problems := len(nums[0])
-	sol1 := 0
+	fmt.Println("part 1:", c(nums, ops))
+	fmt.Println("part 2:", c2(rows[:h-1], rows[h-1]))
+}
 
-	for p := range problems {
-		op := ops[p]
-		result := nums[0][p]
-
+func c(nums [][]int, ops []rune) int {
+	sum := 0
+	for p := 0; p < len(nums[0]); p++ {
+		acc := nums[0][p]
 		for r := 1; r < len(nums); r++ {
-			if op == '+' {
-				result += nums[r][p]
+			if ops[p] == '+' {
+				acc += nums[r][p]
 			} else {
-				result *= nums[r][p]
+				acc *= nums[r][p]
+			}
+		}
+		sum += acc
+	}
+	return sum
+}
+
+func c2(lines []string, opLine string) int64 {
+	var opPos []int
+	for i, c := range opLine {
+		if c == '+' || c == '*' {
+			opPos = append(opPos, i)
+		}
+	}
+	result := int64(0)
+
+	for i := range opPos {
+		start := opPos[i]
+		end := len(opLine)
+		if i+1 < len(opPos) {
+			end = opPos[i+1]
+		}
+
+		var colText []string
+		for _, line := range lines {
+			if start < len(line) {
+				if end > len(line) {
+					end = len(line)
+				}
+				colText = append(colText, line[start:end])
+			} else {
+				colText = append(colText, "")
 			}
 		}
 
-		sol1 += result
+		nums := extractVertical(colText)
+		op := rune(opLine[start])
+
+		result += applyOp(nums, op)
 	}
 
-	fmt.Println("part 1:", sol1)
+	return result
+}
+
+func extractVertical(lines []string) []int {
+	maxH := len(lines)
+	var out []int
+
+	for col := 0; col < len(lines[0]); col++ {
+		var b strings.Builder
+		for row := range maxH {
+			if col < len(lines[row]) && lines[row][col] != ' ' {
+				b.WriteByte(lines[row][col])
+			}
+		}
+		if b.Len() > 0 {
+			n, _ := strconv.Atoi(b.String())
+			out = append(out, n)
+		}
+	}
+	return out
+}
+
+func applyOp(nums []int, op rune) int64 {
+	acc := int64(nums[0])
+	for _, n := range nums[1:] {
+		if op == '+' {
+			acc += int64(n)
+		} else {
+			acc *= int64(n)
+		}
+	}
+	return acc
 }
