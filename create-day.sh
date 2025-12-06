033fddacd696bc192275897ac772f0c2e20f80a5
a9sk <emiliano.rizzonelli@proton.me>
2025-12-06 13:59:24 +0100

    update create-day.sh script

--- create-day.sh
+++ create-day.sh
@@ -8,65 +8,43 @@ fi
 DAY_DIR="year-2025/day-${DAY}"
 
 if [ -d "$DAY_DIR" ]; then
-    echo "Directory '$DAY_DIR' already exists."
+    echo "directory '$DAY_DIR' already exists."
     exit 0
 else
     mkdir "$DAY_DIR"
-    echo "Created directory '$DAY_DIR'."
+    echo "created directory '$DAY_DIR'."
 fi
 
 cat <<EOF > "$DAY_DIR/main.go"
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
 )
 
 func main() {
-	file, err := os.Open("input.txt")
-	if err != nil {
-		fmt.Println("Error opening input file:", err)
-		return
-	}
-	defer file.Close()
+	input, _ := os.ReadFile("input.txt")
 
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading input:", err)
-		return
-	}
+	fmt.Println("part 1:", solvePart1(string(input)))
 
-	input := strings.Join(lines, "\n")
-
-	resultPart1 := solvePart1(string(input))
-	fmt.Println("Solution Part 1:", resultPart1)
-
-	resultPart2 := solvePart2(string(input))
-	fmt.Println("Solution Part 2:", resultPart2)
+	fmt.Println("part 2:", solvePart2(string(input)))
 }
 
 func solvePart1(input string) string {
-
-	return "Solution for Part 1 not implemented"
+	return "no solution yet"
 }
 
 func solvePart2(input string) string {
-
-	return "Solution for Part 2 not implemented"
+	return "no solution yet"
 }
 EOF
 
 touch "${DAY_DIR}/input.txt"
-echo "Initialized input.txt in '$DAY_DIR'."
+echo "initialized input.txt in '$DAY_DIR'."
 
 touch "${DAY_DIR}/test_input.txt"
-echo "Initialized test_input.txt in '$DAY_DIR'."
+echo "initialized test_input.txt in '$DAY_DIR'."
 
-echo "Setup complete for Day ${DAY}!"
+echo "setup complete for day ${DAY}!"
