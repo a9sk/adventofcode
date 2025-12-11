484229d142948ca5e09f5b7a547a57e04813503a
a9sk <emiliano.rizzonelli@proton.me>
2025-12-11 11:05:06 +0100

    solution day 11 part 2

--- year-2025/day-11/main.go
+++ year-2025/day-11/main.go
@@ -7,6 +7,11 @@ import (
 	"strings"
 )
 
+type t struct {
+	s        string
+	fft, dac bool
+}
+
 func main() {
 	input, _ := os.ReadFile("input.txt")
 
@@ -17,6 +22,7 @@ func main() {
 	}
 
 	fmt.Println("part 1:", c(s, s["you"]))
+	fmt.Println("part 2:", c2(s, s["svr"], false, false, map[t]int{}))
 }
 
 func c(m map[string][]string, y []string) (r int) {
@@ -26,6 +32,36 @@ func c(m map[string][]string, y []string) (r int) {
 
 	for _, i := range y {
 		r += c(m, m[i])
+	}
+
+	return r
+}
+
+func c2(m map[string][]string, y []string, fft, dac bool, cache map[t]int) (r int) {
+	if slices.Contains(y, "out") {
+		if fft && dac {
+			return 1
+		}
+		return 0
+	}
+
+	for _, i := range y {
+		nfft, ndac := fft, dac
+		if v, ok := cache[t{i, nfft, ndac}]; ok {
+			r += v
+			continue
+		}
+
+		switch i {
+		case "fft":
+			nfft = true
+		case "dac":
+			ndac = true
+		default:
+		}
+
+		cache[t{i, nfft, ndac}] = c2(m, m[i], nfft, ndac, cache)
+		r += cache[t{i, nfft, ndac}]
 	}
 
 	return r
